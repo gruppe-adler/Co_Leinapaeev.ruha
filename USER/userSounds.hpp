572c0b62dba6336b50472084acb7d5577e4aa703
nomisum <nomisum@googlemail.com>
2019-03-08 23:56:49 +0100

    al bomb added, some additions to airport

--- USER/userSounds.hpp
+++ USER/userSounds.hpp
@@ -22,3 +22,34 @@ class introSiren
     // subtitle delay in seconds, subtitle text
     titles[] = { 0, "" };
 };
+
+class furnal
+{
+    name = "furnal";
+    sound[] = {"\sounds\alias\furnal.ogg", 1, 1};
+    titles[] = {1, ""};
+};
+class burned
+{
+    name = "burned";
+    sound[] = {"\sounds\alias\burned.ogg", 1, 1};
+    titles[] = {1, ""};
+};
+class tipat
+{
+    name = "tipat";
+    sound[] = {"\sounds\alias\tipat.ogg", 0.8, 1};
+    titles[] = {1, ""};
+};
+class bombix
+{
+    name = "bombix";
+    sound[] = {"\sounds\alias\bombix.ogg", db+20, 1};
+    titles[] = {1, ""};
+};
+class bombix_echo
+{
+    name = "bombix_echo";
+    sound[] = {"\sounds\alias\bombix_echo.ogg", db+5, 1};
+    titles[] = {1, ""};
+};
